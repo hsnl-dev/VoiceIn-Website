4f54b782c05492a048577bfcd2bfb048458a21ad
Henry <qoo392@hotmail.com>
2016-05-03 21:53:05 +0800

    add debugger

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const debug = require('debug');
+const debug = require('debug')('app:appjs');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
