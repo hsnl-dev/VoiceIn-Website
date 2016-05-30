3c8f2e142d4d57bc041e45ae23d5f8c2fb949147
lockys <locky4567@gmail.com>
2016-05-30 22:10:11 +0800

    change production route.

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ module.exports = exports = {
   },
   production: {
     url: process.env.isProduction === 'true' ?
-    'mongodb://hsnl:hsnl33564@ds019882.mlab.com:19882/voicein-production'
+    'mongodb://hsnl-dev:hsnl33564hsnl33564@ds021969.mlab.com:21969/voicein-production'
     : 'mongodb://hsnl-dev:hsnl33564hsnl33564@ds013908.mlab.com:13908/voicein',
   },
 };
